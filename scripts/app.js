28baa7292c7b473e927777f81651d4ba2a0cd434
Sinead Cheung <sineadccheung@gmail.com>
2017-04-18 21:55:47 -0500

    including angular router

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,21 @@
+<!doctype html>
+
+<html ng-app = "myApp">
+
+  <head>
+    <script src="scripts/angular-ui-router.js"></script>
+    <script>
+    var app = angular.module("myApp", [ui.router]);
+    </script>
+  </head>
+
+  <body></body>
+</html>
+
+
+
+
+
 $stateProvider.state('contacts', {
   template: '<h1>My Contacts</h1>'
 })
