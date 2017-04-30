bad2627342d7838ccc2f33212fd52c4f5792fcf5
Sinead Cheung <sineadccheung@gmail.com>
2017-04-29 18:39:25 -0700

    restart

--- scripts/controllers/Room.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  function Room($firebaseArray) {
-    var ref = firebase.database().ref();
-  }
-
-  angular
-    .module('blocChat')
-    .factory('Room', ['$firebaseArray', Room]);
-})();
